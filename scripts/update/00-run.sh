dd4601b53efe2bb32fe533945b9217e35325c3db
Luke Childs <lukechilds123@gmail.com>
2020-09-25 23:42:24 +0700

    Fail updates on low memory devices

--- scripts/update/00-run.sh
+++ scripts/update/00-run.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 RELEASE=$1
 UMBREL_ROOT=$2
 
+./check-memory "${UMBREL_ROOT}"
+
 echo
 echo "======================================="
 echo "=============== UPDATE ================"
